1e3b1e9df93fcc2c0a8ba406ef08ff290f04c38b
Denis Ilyushin <to@denisilyushin.ru>
2023-07-28 22:37:13 +0300

    fix: tests4

--- controllers/user.js
+++ controllers/user.js
@@ -47,7 +47,7 @@ module.exports.getUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const userId = req.user._id
-  let userInfo = {}
+  let userInfo
 
   // проверка обновляемых параметров по пути запроса
   if (req.path.includes('avatar')) {
