7833042e2344f2dba4762958c87bd666ebdbf901
Denis Ilyushin <to@denisilyushin.ru>
2023-07-28 19:43:38 +0300

    feat: сделать URI
    PATCH /users/me — обновляет профиль
    PATCH /users/me/avatar — обновляет аватар

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const {
   StatusCodes
 } = require('http-status-codes');
 const mongoose = require('mongoose');
+const itemNotFound = require('../errors/errors.js');
 
 module.exports.createUser = (req, res) => {
   User.create(req.body)
@@ -27,7 +28,7 @@ module.exports.createUser = (req, res) => {
           })
       }
     })
-}
+};
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
@@ -41,7 +42,7 @@ module.exports.getAllUsers = (req, res) => {
           message: error.message ? error.message : 'Непредвиденная ошибка сервера'
         })
     })
-}
+};
 
 module.exports.getUser = (req, res) => {
   const userId = req.params.id
@@ -52,7 +53,6 @@ module.exports.getUser = (req, res) => {
         .send(user)
     })
     .catch((error) => {
-      console.log(error)
       if (error instanceof mongoose.Error.CastError) {
         res
           .status(StatusCodes.NOT_FOUND)
@@ -68,4 +68,59 @@ module.exports.getUser = (req, res) => {
           })
       }
     })
-}
+};
+
+module.exports.updateUser = (req, res) => {
+  const userId = req.user._id
+  let userInfo = {}
+
+  // проверка обновляемых параметров по пути запроса
+  if (req.path.includes('avatar')) {
+    userInfo = { avatar: req.body.avatar }
+  } else {
+    userInfo = {
+      name: req.body.name,
+      about: req.body.about
+    }
+  }
+
+  User.findByIdAndUpdate(userId, userInfo, {
+    new: true,
+    runValidators: true,
+    upsert: false
+  })
+    .orFail(() => {
+      throw itemNotFound(
+        `Информацию о пользователе с ID ${userId} невозможно обновить.
+         Пользователь не найден`
+      )
+    })
+    .then((user) => {
+      res
+        .status(StatusCodes.OK)
+        .send(user)
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.CastError) {
+        res
+          .status(StatusCodes.NOT_FOUND)
+          .send({
+            message: `Пользователь с ID ${userId} не найден`,
+            details: error.message ? error.message : ''
+          })
+      } else if (error instanceof itemNotFound) {
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .send({
+            message: `Не удалось обновить информацию пользователя. Данные не валидны`,
+            details: error.message ? error.message : ''
+          })
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .send({
+            message: error.message ? error.message : 'Непредвиденная ошибка сервера'
+          })
+      }
+    })
+};
