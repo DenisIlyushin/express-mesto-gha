b3cf6c37fdbb7ba6b62cd743ec1f6cf7bbc2e6dd
Denis Ilyushin <to@denisilyushin.ru>
2023-07-28 15:21:10 +0300

    feat: добавить модели

---
+++ models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
+  },
+  avatar: {
+    type: String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('user', userSchema);
